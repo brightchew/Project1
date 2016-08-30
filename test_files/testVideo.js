13887fbd0c7334bfe8918e536b0c67fe39403390
brightchew <bright@homeloan.com.sg>
2016-08-30 17:23:56 +0800

    added files for testing out BigVideo library

---
+++ test_files/testVideo.js
@@ -0,0 +1,6 @@
+$(function() {
+    var BV = new $.BigVideo();
+    BV.init();
+    // BV.show('http://vjs.zencdn.net/v/oceans.mp4',{ambient:true});
+   	BV.show('https://www.youtube.com/watch?v=9No-FiEInLA');
+});
