bb7d94fa16b681bfa93c2d0a70d197a86161758c
brightchew <bright@homeloan.com.sg>
2016-08-31 17:07:20 +0800

    add start button and make it works

---
+++ layoutTest.js
@@ -0,0 +1,146 @@
+console.log('javascript linked');
+
+$( "document" ).ready(function() {
+        console.log( "document loaded" );
+    });
+ 
+ $("#start").on("click", function() {
+			renderQuestion();
+    			startTimer();
+    			$( "#start" ).prop( "disabled", true );
+ 
+
+    			console.log("started Jquery");
+
+ })
+
+ var pos = 0,//pos is the question position in Array file
+    test, test_status, question, choice, choices, chA, chB, chC, correct = 0;
+
+var questions = [
+    ["what is 10 + 4 ?", "12", "14", "16", "B", "img/1.png"],
+    ["What is 20 - 9 ?", "7", "13", "11", "C","img/2.png"],
+    ["What is 7 x 3 ?", "21", "24", "25", "A", "img/3.png"],
+    ["What is 8 / 2 ?", "10", "2", "4", "C", "img/4.png"]
+];
+
+function element(x) {
+    return document.getElementById(x);
+}
+
+function renderQuestion() {
+    test = element("test"); //get element of form div;
+
+    // the following code is to check if question reach the end;
+
+    
+
+    if (pos >= questions.length || timeInSeconds === 0) {
+
+    	// Check if game is over;
+
+        test.innerHTML = "<h2> You got " + correct + " of " + questions.length + " questions correct </h2>";
+        element("test_score").textContent = "SCORE: " + correct + "/" +questions.length;
+        
+        element("test_status").innerHTML = "test completed";
+
+        // if game is over, reset game;
+        pos = 0;
+        correct = 0;
+        progressBar = 0;
+        return false;
+        
+
+    } else {
+
+     element("test_score").textContent = "SCORE: " + correct + "/" +questions.length;
+
+    }
+
+    element("test_status").textContent = "Question of " + (pos+1) + " of " + questions.length;
+
+    progressBar = Math.floor(((pos+1)/questions.length)*100) + "%";
+
+    console.log("progressbar is " + progressBar);
+
+    $("#progressBar").css("width", progressBar);
+
+    question = questions[pos][0];
+    chA = questions[pos][1];
+    chB = questions[pos][2];
+    chC = questions[pos][3];
+
+    
+
+    //write the questions in form
+
+
+
+     var imgFile = questions[pos][5];
+
+     console.log("imgFile= " + imgFile)
+
+    test.innerHTML = "<h3>" + question + "</h3>";
+    test.innerHTML += "<img src='"+imgFile +"'" + "></img> <br>";
+    test.innerHTML += "<input type='radio' name='choices' value='A'>" + chA + "<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='B'>" + chB + "<br>";
+    test.innerHTML += "<input type='radio' name='choices' value='C'>" + chC + "<br>";
+    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer </button>";
+}
+    
+
+function checkAnswer() {
+
+
+    choices = document.getElementsByName('choices');
+    // choices is an array 
+
+    for (var i = 0; i < choices.length; i++ ) {
+        if (choices[i].checked) {
+            choice = choices[i].value;
+        }
+    }
+
+        if (choice == questions[pos][4]) {
+            correct++;
+        }
+        pos++;
+        renderQuestion();
+
+}
+
+//Timer Function
+
+timeInSeconds = 60;
+
+function countdown() {
+
+if (timeInSeconds>0) {
+  timeInSeconds--
+  minutes = Math.floor(timeInSeconds/60);
+  
+  seconds = (timeInSeconds % 60)
+  console.log("minutes: " + minutes + " seconds: " + seconds)
+  document.getElementById("timer").textContent = "Time Remaining " + minutes + ":" + seconds;
+} else { 
+    return false;
+
+}}
+
+console.log( "position after countdown()");
+
+
+function startTimer () {    
+    document.getElementById("startTimer").addEventListener('click', function() {
+      timerID = setInterval(countdown, 1000)
+    })
+}
+
+// //Restart
+// $("#restart").on("click", function() {
+// 	pos = 0;
+//     correct = 0;
+//     progressBar = 0;
+//     $("test img").css("src", "img/cover.png");
+//     $( "#start" ).prop( "disabled", false );
+// }
